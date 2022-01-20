c9e87a103a106d303392b1457bc37c90463bf0db
vilay <kandi.vilay09@gmail.com>
2022-01-20 20:07:07 +0530

    Feat  : added manager details to cooperative

--- src/cropcert/user/model/Cooperative.java
+++ src/cropcert/user/model/Cooperative.java
@@ -10,45 +10,82 @@ import javax.xml.bind.annotation.XmlRootElement;
 import io.swagger.annotations.ApiModel;
 
 @Entity
-@Table (name = "cooperative")
+@Table(name = "cooperative")
 @XmlRootElement
-@PrimaryKeyJoinColumn(name="id")
-@DiscriminatorValue(value= "co")
+@PrimaryKeyJoinColumn(name = "id")
+@DiscriminatorValue(value = "co")
 @ApiModel("Cooperative")
-public class Cooperative extends CropcertEntity{
+public class Cooperative extends CropcertEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1216212900838354949L;
 
-	@Column (name = "union_code")
+	@Column(name = "union_code")
 	private Long unionCode;
-	
+
 	@Column(name = "num_farmer")
 	private Long numFarmer;
-	
+
 	@Column(name = "farmer_seq_number")
 	private Long farSeqNumber;
-	
+
+	@Column(name = "full_name")
+	private String fullName;
+
+	@Column(name = "manager")
+	private String manager;
+
+	@Column(name = "contact")
+	private String contact;
+
 	public Long getUnionCode() {
 		return unionCode;
 	}
+
 	public void setUnionCode(Long unionCode) {
 		this.unionCode = unionCode;
 	}
-	
+
 	public Long getNumFarmer() {
 		return numFarmer;
 	}
+
 	public void setNumFarmer(Long numFarmer) {
 		this.numFarmer = numFarmer;
 	}
-	
+
 	public Long getFarSeqNumber() {
 		return farSeqNumber;
 	}
+
 	public void setFarSeqNumber(Long farSeqNumber) {
 		this.farSeqNumber = farSeqNumber;
 	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getManager() {
+		return manager;
+	}
+
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
 }
